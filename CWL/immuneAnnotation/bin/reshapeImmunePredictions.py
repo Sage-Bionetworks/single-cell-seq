02a790fdcd64a85adc44a2da434a2453b70724ad
Sara JC Gosline <sara.gosline@sagebase.org>
2019-07-29 21:33:59 -0700

    updated reshape command

--- CWL/immuneAnnotation/bin/reshapeImmunePredictions.py
+++ CWL/immuneAnnotation/bin/reshapeImmunePredictions.py
@@ -10,7 +10,20 @@ from optparse import OptionParser
 reshapeDf
 takes a data frame from immclass and reshapes to immune annotation data model
 '''
-def reshapeImmClassDf(df):
+def reshapeImmClassDf(df,training,dn,tumorType='none'):
+    tab=pd.read_csv(df,sep='\t')
+    preds=[k.split(':')[1] for k in tab.auto]
+    gens=tab.general
+    newdf=pd.DataFrame(columns=['Dataset name','Cell identifier','Cell Prediction','Algorithm','Tumor Type','Training File'])
+    newdf['Dataset name']=['']*len(preds)
+    newdf['Algorithm']=['ImmClassifier']*len(preds)
+    newdf['Training File']=[training]*len(preds)
+    newdf['Cell Prediction']=preds
+    newdf['Cell identifier']=gens.keys()
+    newdf['Dataset name']=[dn]*len(preds)
+    newdf.to_csv()
+
+
 
 
 
@@ -21,6 +34,9 @@ def main():
     parser.add_option("-m", "--modelUsed",
                    dest="model", default=['immClassifier','garnett'],
                   help="don't print status messages to stdout")
+    parser.add_option('-t','--tumorType',
+                      dest='tumorType',default='none')
+    parser.add_option('-r','--trainingFile',help='SynID of file used in training')
 
     (options, args) = parser.parse_args()
 
